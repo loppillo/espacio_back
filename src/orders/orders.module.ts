28fca4c23488459fea86c9ec57daedd92d04decf
Francisco Poblete <36689098+loppillo@users.noreply.github.com>
2025-10-02 17:55:23 -0300

    ProductOrder typeorm

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -8,10 +8,11 @@ import { Customer } from 'src/customer/entities/customer.entity';
 import { Product } from 'src/products/entities/product.entity';
 import { Mesa } from 'src/mesas/entities/mesa.entity';
 import { Propina } from 'src/propina/entities/propina.entity';
+import { ProductsOrders } from 'src/products-orders/entities/products-order.entity';
 
 @Module({
   imports:[TypeOrmModule.forFeature([Order]),TypeOrmModule.forFeature([User]),TypeOrmModule.forFeature([Propina]),TypeOrmModule.forFeature([Customer]),TypeOrmModule.forFeature([Product]),
-  TypeOrmModule.forFeature([Mesa])
+  TypeOrmModule.forFeature([Mesa]),TypeOrmModule.forFeature([ProductsOrders])
 ],
   controllers: [OrdersController],
   providers: [OrdersService],
