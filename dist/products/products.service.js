e6cc5ee4e252dcbbef7bb7a2b5c1295abe464d07
Francisco Poblete <36689098+loppillo@users.noreply.github.com>
2025-10-06 17:39:59 -0300

    build

--- dist/products/products.service.js
+++ dist/products/products.service.js
@@ -99,19 +99,20 @@ let ProductsService = class ProductsService {
         page = Math.max(1, page);
         limit = Math.max(1, limit);
         const skip = (page - 1) * limit;
-        const query = this.proRepository
-            .createQueryBuilder('product')
+        const query = this.proRepository.createQueryBuilder('product')
             .leftJoinAndSelect('product.categories', 'category')
-            .orderBy('product.id', 'DESC')
-            .skip(skip)
-            .take(limit);
+            .orderBy('product.id', 'DESC');
         if (nombre) {
-            query.andWhere('product.name LIKE :nombre', { nombre: `%${nombre}%` });
+            query.andWhere('LOWER(product.name) LIKE LOWER(:nombre)', { nombre: `%${nombre}%` });
         }
         if (categoryId) {
             query.andWhere('category.id = :categoryId', { categoryId });
         }
-        const [productos, total] = await query.getManyAndCount();
+        const total = await query.getCount();
+        const productos = await query
+            .skip(skip)
+            .take(limit)
+            .getMany();
         const data = productos.map((producto) => ({
             id: producto.id,
             name: producto.name,
