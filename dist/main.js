4a9119db9a4aa8b89098db428257eca1e6ba61e0
Francisco Poblete <36689098+loppillo@users.noreply.github.com>
2025-10-29 12:24:32 -0300

    build

--- dist/main.js
+++ dist/main.js
@@ -31,7 +31,11 @@ const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.setGlobalPrefix("api/v1");
-    app.enableCors();
+    app.enableCors({
+        origin: 'https://espacioboulevardlinares.cl',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        allowedHeaders: 'Authorization,Content-Type',
+    });
     app.useGlobalPipes(new common_1.ValidationPipe({
         whitelist: true,
         forbidNonWhitelisted: true,
