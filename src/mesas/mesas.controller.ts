63f0cb695efb5b59560b643a5b5b8061d0cb69c8
Francisco Poblete <36689098+loppillo@users.noreply.github.com>
2025-11-03 13:01:38 -0300

    solucion

--- src/mesas/mesas.controller.ts
+++ src/mesas/mesas.controller.ts
@@ -24,52 +24,46 @@ import { RolesGuard } from 'src/roles/roles.guard';
 export class MesaController {
   constructor(private readonly mesaService: MesaService) {}
   @Get(':id/detalle-actual')
-  @UseGuards(JwtAuthGuard, RolesGuard)
 async getDetalleMesaActual(@Param('id', ParseIntPipe) id: number) {
   return this.mesaService.obtenerDetalleMesaActual(id);
 }
 
 
     @Get('historial')
-    @UseGuards(JwtAuthGuard, RolesGuard)
 async getHistorialPorMesas(@Query('mesaId', ParseIntPipe) mesaId: number) {
   return this.mesaService.getPedidosPorMesa(mesaId);
 }
 
 
   @Get()
-  @UseGuards(JwtAuthGuard, RolesGuard)
   findAll(): Promise<Mesa[]> {
     return this.mesaService.findAll();
   }
 
     @Get('/obtener/:id')
-    @UseGuards(JwtAuthGuard, RolesGuard)
+    
   async obtenerMesaPorId(@Param('id', ParseIntPipe) id: number) {
     return await this.mesaService.obtenerMesaPorId(id);
   }
 
   @Get('/detalle/:id')
-  @UseGuards(JwtAuthGuard, RolesGuard)
+
   async getDetalleMesa(@Param('id', ParseIntPipe) id: number): Promise<Mesa> {
     return this.mesaService.obtenerDetalleMesa(id);
   }
 
 
   @Get(':id')
-  @UseGuards(JwtAuthGuard, RolesGuard)
   findOne(@Param('id', ParseIntPipe) id: number): Promise<Mesa> {
     return this.mesaService.findOne(id);
   }
 
   @Post()
-  @UseGuards(JwtAuthGuard, RolesGuard)
   create(@Body() createMesaDto: CreateMesaDto): Promise<Mesa> {
     return this.mesaService.create(createMesaDto);
   }
 
   @Put(':id')
-  @UseGuards(JwtAuthGuard, RolesGuard)
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateMesaDto: UpdateMesaDto,
@@ -78,13 +72,11 @@ async getHistorialPorMesas(@Query('mesaId', ParseIntPipe) mesaId: number) {
   }
 
   @Delete(':id')
-  @UseGuards(JwtAuthGuard, RolesGuard)
   remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return this.mesaService.remove(id);
   }
 
   @Put(':id/estado')
-  @UseGuards(JwtAuthGuard, RolesGuard)
   async actualizarEstadoMesa(
     @Param('id') id: number,
     @Body('status') status: string,
@@ -93,14 +85,12 @@ async getHistorialPorMesas(@Query('mesaId', ParseIntPipe) mesaId: number) {
   }
 
   @Patch(':id/pagar')
-  @UseGuards(JwtAuthGuard, RolesGuard)
 async marcarPedidoPagado(@Param('id') mesaId: number) {
   return await this.mesaService.marcarPedidoPagado(mesaId);
 }
 
 
   @Get(':id')
-  @UseGuards(JwtAuthGuard, RolesGuard)
   getMesa(@Param('id') id: number): Promise<Mesa> {
     return this.mesaService.getMesa(id);
   }
@@ -109,13 +99,11 @@ async marcarPedidoPagado(@Param('id') mesaId: number) {
  
 
   @Post(':id/nuevo-pedido')
-  @UseGuards(JwtAuthGuard, RolesGuard)
   crearNuevoPedido(@Param('id') id: number): Promise<Order> {
     return this.mesaService.crearNuevoPedido(id);
   }
 
   @Get(':id/pedidos')
-  @UseGuards(JwtAuthGuard, RolesGuard)
   getPedidosActuales(
     @Param('id') id: number,
     @Query('numeroVenta') numeroVenta: number,
@@ -124,7 +112,6 @@ async marcarPedidoPagado(@Param('id') mesaId: number) {
   }
 
 @Get('ventas/detalle-mesa')
-@UseGuards(JwtAuthGuard, RolesGuard)
 async getDetalleMesas(
   @Query('mesaId') mesaId: number,
   @Query('fecha') fecha?: string,
