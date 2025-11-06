1f322c2b4265305e30625d236aaf025d9a29d21e
Francisco Poblete <36689098+loppillo@users.noreply.github.com>
2025-11-06 14:14:08 -0300

    dto

--- src/categoria-gasto/dto/create-categoria-gasto.dto.ts
+++ src/categoria-gasto/dto/create-categoria-gasto.dto.ts
@@ -1,1 +1,8 @@
-export class CreateCategoriaGastoDto {}
+import { IsString, IsNotEmpty, MaxLength } from 'class-validator';
+
+export class CreateCategoriaGastoDto {
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre es obligatorio.' })
+  @MaxLength(100, { message: 'El nombre no puede superar los 100 caracteres.' })
+  nombre: string;
+}
