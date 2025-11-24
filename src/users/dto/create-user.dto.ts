f8d86fc2b85066202b6304ee898a3894df9e67ad
willnout <willnout@gmail.com>
2025-11-24 19:02:35 -0300

    refactor: Rename user `name` field to `username` in DTO and entity.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,7 +2,7 @@ import { IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
-  name: string;
+  username: string;
 
   @IsString()
   full_name: string;
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -7,7 +7,7 @@ export class User {
   id: number;
 
   @Column()
-  name: string;
+  username: string;
 
   @Column()
   full_name: string;
