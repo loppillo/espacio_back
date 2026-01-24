71fe9df3606006da1f1944e0e48ae590df28c270
loppillo <franpobletron@gmail.com>
2026-01-24 11:30:47 -0300

    feat: add DocumentoIngreso entity to IngresosModule's TypeOrm feature list.

--- src/ingresos/ingresos.module.ts
+++ src/ingresos/ingresos.module.ts
@@ -4,11 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ingreso } from './entities/ingreso.entity';
 import { CategoriaIngreso } from 'src/categoria_ingresos/entities/categoria_ingreso.entity';
 import { ClienteIngreso } from 'src/clientes_ingresos/entities/cliente_ingreso.entity';
+import { DocumentoIngreso } from 'src/documentos_ingreso/entities/documento_ingreso.entity';
 import { IngresoController } from './ingresos.controller';
 import { IngresoService } from './ingresos.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Ingreso, CategoriaIngreso, ClienteIngreso])],
+    imports: [TypeOrmModule.forFeature([Ingreso, CategoriaIngreso, ClienteIngreso, DocumentoIngreso])],
     controllers: [IngresoController],
     providers: [IngresoService],
     exports: [IngresoService],
