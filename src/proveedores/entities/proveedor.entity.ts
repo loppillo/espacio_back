749c6b263946c8a5bcce77585c459f31cb07dbd1
loppillo <franpobletron@gmail.com>
2026-02-02 16:24:37 -0300

    modificacion de emaul en proveedores

--- src/proveedores/entities/proveedor.entity.ts
+++ src/proveedores/entities/proveedor.entity.ts
@@ -21,7 +21,7 @@ export class Proveedor {
     @Column({ nullable: false })
     telefono: string;
 
-    @Column({ nullable: false })
+    @Column()
     email: string;
 
     @OneToMany(() => Gasto, (gasto) => gasto.proveedor)
